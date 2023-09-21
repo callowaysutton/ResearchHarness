8850fee19e4cbe0e7365f99c65283d7867fd3d2c
callowaysutton <me@callowaysutton.com>
2023-09-21 16:21:04 +0000

    Get the skeleton project up

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='research-harness',
+    version='0.1',
+    description='A research harness for running experiments',
+    author='Calloway Sutton and Antonio Lee',
+    author_email='me@callowaysutton.com',
+    packages=find_packages(),
+)
